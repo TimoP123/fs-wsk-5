2c75dfa1c422fcc818281bd4ed995534bf40cbca
Timo Pudas <timo.pudas@helsinki.fi>
2018-03-01 22:47:39 +0200

    cleaning up

--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Header = () => (
-  <div>
-    <h1>Blog list application</h1>
-  </div>
-);
-
-export default Header;
--- src/components/Wrapper.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Wrapper extends React.Component {
-  render() {
-    return <div className={"wrapper"}>{this.props.children}</div>;
-  }
-}
-
-export default Wrapper;
